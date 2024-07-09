a5acfa0563abc24bbad8802170123eb799594139
IBetULookGood <alekseychick.tim@yandex.ru>
2024-07-09 19:22:53 +0300

    try to obtain good ratio

--- search_medium/search.cpp
+++ search_medium/search.cpp
@@ -3,4 +3,46 @@
 bool search(const std::vector<int>& data, int value)
 {
     // Your code goes here.
+    // int left = 0;
+    // int right = data.size()-1;
+    // int mid1 = 0;
+    // int mid2 = 0;
+    // while(left <= right){
+    //     mid1 = left + (right - left) / 3;
+    //     mid2 = right - (right - left) / 3;
+    //     if(data[mid1] == value)
+    //         return true;
+    //     if(data[mid2] == value)
+    //         return true;
+    //     if(value < data[mid1]){
+    //         right = mid1 - 1;
+    //     }else if(value > data[mid2]){
+    //         left = mid2 + 1;
+    //     }else{
+    //         left = mid1 + 1;
+    //         right = mid2 - 1;
+    //     }
+    // }
+    // return false;
+
+
+    // int base = 0;
+    // int len = data.size();
+    // while(len > 1){
+    //     int half = len / 2;
+    //     base += (data[base + half - 1] < value) * half;
+    //     len -= half;
+    // } 
+    // return data[base] == value;
+
+    int base = 0;
+    int len = data.size();
+    while(len > 1){
+        int half = len / 2;
+        len -= half;
+        __builtin_prefetch(&data[len / 2 - 1]);
+        __builtin_prefetch(&data[half + len / 2 - 1]);
+        base += (data[base + half - 1] < value) * half;
+    } 
+    return data[base] == value;
 }
--- search_small/search.cpp
+++ search_small/search.cpp
@@ -1,6 +1,55 @@
 #include "search.h"
+#include <immintrin.h>
 
 bool search(const std::vector<int>& data, int value)
 {
     // Your code goes here.
+
+    
+    int first = 0;
+    int last = data.size();
+    int length = last;
+    constexpr int entries_per_256KB = 256 * 1024 / sizeof(data);
+    if (length >= entries_per_256KB) {
+        constexpr int num_per_cache_line = std::max(64 / int(sizeof(data)), 1);
+        while (length >= 3 * num_per_cache_line) {
+            int half = length / 2;
+            __builtin_prefetch(&data[first + half / 2]);
+            int first_half1 = first + (length - half);
+            __builtin_prefetch(&data[first_half1 + half / 2]);
+            first = data[first + half] < value ? first_half1 : first;
+            length = half;
+        }
+    }
+    while (length > 0) {
+        int half = length / 2;
+        int first_half1 = first + (length - half);
+        first = data[first + half]< value ? first_half1 : first;
+        length = half;
+    }
+    return data[first]==value;
+
+
+
+    // std::vector<int> t(data);
+    // t.push_back(2147483647);
+    // int k = 1;
+    // int n = data.size();
+    // while(k <= n){
+    //     __builtin_prefetch(&t[k*2]);
+    //     k = 2 * k + (t[k] < value);
+    // }
+    // k >>= __builtin_ffs(~k);
+    // return t[k]==value;
+
+    // int base = 0;
+    // int len = data.size();
+    // while(len > 1){
+    //     int half = len / 2;
+    //     len -= half;
+    //     __builtin_prefetch(&data[len / 2 - 1]);
+    //     __builtin_prefetch(&data[half + len / 2 - 1]);
+    //     base += (data[base + half - 1] < value) * half;
+    // } 
+    // return data[base] == value;
 }
