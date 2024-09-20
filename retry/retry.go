f1579b1df2d237a75c5d24f2074a7e1713d3e91d
IBetULookGood <alekseychick.tim@yandex.ru>
2024-09-20 19:08:38 +0300

    attempt 1

--- retry/retry.go
+++ retry/retry.go
@@ -1,17 +1,38 @@
 package main
 
 import (
+    "math/rand"
     "time"
 )
 
 func retry(args Args, packets chan Packet, rps int) {
     response := make(chan error)
     packet := Packet{args: args, response: response}
-    timeout := time.Second
+    
+    baseTimeout := time.Second / time.Duration(rps)
+    maxTimeout := 4 * time.Second 
+    
+    attempt := 0
     for {
-        time.Sleep(timeout)
         if send(packet, packets) {
             break
         }
+        
+        attempt++
+        timeout := exponentialBackoff(baseTimeout, attempt, maxTimeout)
+        time.Sleep(timeout)
     }
+}
+
+func exponentialBackoff(baseTimeout time.Duration, attempt int, maxTimeout time.Duration) time.Duration {
+    timeout := baseTimeout * time.Duration(1<<uint(attempt)) 
+    
+    jitter := time.Duration(rand.Float64() * float64(timeout) * 0.25)
+    timeout += jitter
+    
+    if timeout > maxTimeout {
+        timeout = maxTimeout
+    }
+    
+    return timeout
 }
